ced1afe075e2db5925335f0d17f09f30884ea3e1
Cihad Aydemir <cihad.aydemir@mhp.com>
2023-03-19 09:33:36 +0100

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div>
+      <h2>Oops</h2>
+      <p>Something went wrong!</p>
+      <p>Please try again later!</p>
+    </div>
+  );
+}
