814b63536f02809182d7e27dbf5db995e5fb0552
Cihad Aydemir <cihad.aydemir@mhp.com>
2023-03-26 17:56:52 +0200

    feat: add add button to card for favorizing

--- src/app/components/PokeCard.tsx
+++ src/app/components/PokeCard.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { Add } from "@mui/icons-material";
+import { IconButton, Stack } from "@mui/material";
 import Image from "next/image";
 import { capitalizeFirstLetter } from "../lib/utils";
 
 export const PokeCard = ({ name, index }: { name: string; index: number }) => {
   return (
     <div className="pokecard">
-      <p style={{ textAlign: "center" }}>{capitalizeFirstLetter(name)}</p>
+      <Stack direction={"row"} alignItems={"center"}>
+        <p style={{ textAlign: "center" }}>{capitalizeFirstLetter(name)}</p>
+        <IconButton style={{ justifySelf: "flex-end" }}>
+          <Add color="warning" />
+        </IconButton>
+      </Stack>
+
       <Image
         alt={name}
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
