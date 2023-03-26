5133873d557e2bfe9a0762b75b0cc1ba2db5de88
Cihad Aydemir <cihad.aydemir@mhp.com>
2023-03-26 17:54:26 +0200

    feat: add github user to db on sign in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/app/lib/prisma";
+import { Session } from "@/app/types/session";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -10,6 +12,44 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      let maybeUser = await prisma.user.findUnique({ where: { id: +user.id } });
+      if (!maybeUser) {
+        maybeUser = await prisma.user.create({
+          data: {
+            id: +user.id,
+            email: user.email,
+          },
+        });
+      }
+      console.log(maybeUser);
+
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token, user }) {
+      const sess: Session = {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id as string,
+          role: token.role as string,
+        },
+      };
+
+      return sess;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
